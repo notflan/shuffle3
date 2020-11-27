52a8be1d0c0648036337329dbccb6d5faa9ec96b
Avril <flanchan@cumallover.me>
2020-11-27 15:00:51 +0000

    pretty panic and debug

--- lean/src/debug.c
+++ lean/src/debug.c
@@ -4,12 +4,14 @@
 
 #include <debug.h>
 
+#include <colours.h>
+
 void _do_dprintf(struct debuginfo info, const char* fmt, ...)
 {
 #ifdef DEBUG
 	va_list li;
 	va_start(li, fmt);
-	fprintf(stderr, "[dbg @%s->%s:%d]: ", info.file, info.function,info.line);
+	fprintf(stderr, "["FGRN("dbg")" " FYEL("@") BOLD("%s") "->"  FWHT("%s") ":" FYEL("%d") "]: ", info.file, info.function,info.line);
 	vfprintf(stderr, fmt, li);
 	fprintf(stderr, "\n");
 	va_end(li);
--- lean/src/panic.c
+++ lean/src/panic.c
@@ -4,11 +4,13 @@
 
 #include <panic.h>
 
+#include <colours.h>
+
 __attribute__((noreturn)) void _do_panic(struct panicinfo info, const char* fmt, ...)
 {
 	va_list li;
 	va_start(li, fmt);
-	 fprintf(stderr, "[!] (%s->%s:%d) fatal error: ", info.file, info.function, info.line);
+	 fprintf(stderr, BOLD(UNDL(FRED("[!]"))) " (" BOLD("%s") "->" BOLD(FRED("%s")) ":" FYEL("%d") ") " BOLD(FRED("fatal error")) ": ", info.file, info.function, info.line);
 	vfprintf(stderr, fmt, li);
 	 fprintf(stderr, "\n");
 	va_end(li);
